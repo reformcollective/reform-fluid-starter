4a57f162cd549d82dbe4e11f4cee71a30058b288
Blake Wight <blake@fluid.app>
2024-08-09 14:14:15 -0600

    fixed url issue

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,10 +1,6 @@
 const FluidApiClient = async (url: string, options?: RequestInit) => {
   const response = await fetch(
-    `${
-      process.env.FLUID_BASE_URL
-        ? `${process.env.FLUID_BASE_URL}/api/company/v1/${url}`
-        : "http://fluid.app/api/company/v1/"
-    }${url}`,
+    `${process.env.FLUID_BASE_URL || "http://fluid.app"}/api/company/v1/${url}`,
     {
       ...options,
       headers: {
@@ -15,10 +11,13 @@ const FluidApiClient = async (url: string, options?: RequestInit) => {
   );
 
   // instead of having to async get the body from every client call, we can just do it here
-  const body = await response.json();
   return {
     ...response,
-    body,
+    body: await (response.headers
+      .get("content-type")
+      ?.includes("application/json")
+      ? response.json()
+      : response.text()),
   };
 };
 
