749ccee2c7c71c7a8841eabc3dad2d78c9c9d129
Anand Shrestha <anandsthajp@gmail.com>
2024-12-04 16:28:10 +0900

    add: bg gradient tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       screens: {
         "2xl": "1440px", // Custom breakpoint for large screens
       },
