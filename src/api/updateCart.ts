6fca792f0dba47653a2fdf31b5971ea1a854c8e3
YogishaNiraula <yogishaniraula@gmail.com>
2024-12-05 10:29:23 +0545

    chore: comment

--- src/api/updateCart.ts
+++ src/api/updateCart.ts
@@ -4,6 +4,7 @@ import client from "@/api/client";
 import { Carts, cartSchema } from "@/types/cart";
 import { cookies } from "next/headers";
 
+// TODO: update the API body and function params
 async function updateCart(changedData: Record<string, any>): Promise<Carts> {
   const cookiesList = cookies();
   const cartToken = cookiesList.get("cartToken")?.value;
